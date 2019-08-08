9319f7aae620535bee248041e5733c9eb3f6cdec
dunqan <dunqan@w9.pl>
2019-08-08 11:04:34 +0200

    cleaning it up

--- webpack.config.base.js
+++ webpack.config.base.js
@@ -1,18 +1,8 @@
-const DotenvPlugin = require('dotenv-webpack')
 
 module.exports = {
   entry: './src/index.js',
   module: {
     rules: [
-      // {
-      //   test: /\.(js|jsx)$/,
-      //   enforce: 'pre',
-      //   include: /src/,
-      //   loader: 'eslint-loader',
-      //   options: {
-      //     emitWarning: process.env.NODE_ENV !== 'production',
-      //   },
-      // },
       {
         test: /\.(js|jsx)$/,
         include: /src/,
@@ -24,7 +14,6 @@ module.exports = {
       },
     ],
   },
-  plugins: [new DotenvPlugin()],
 
   resolve: {
     extensions: ['.js', '.json', '.jsx'],
--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,8 +1,7 @@
-const path = require('path')
-const webpack = require('webpack')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const merge = require('webpack-merge')
-const base = require('./webpack.config.base')
+const path = require('path');
+const webpack = require('webpack');
+const merge = require('webpack-merge');
+const base = require('./webpack.config.base');
 
 module.exports = merge(base, {
   mode: 'development',
@@ -12,44 +11,11 @@ module.exports = merge(base, {
     chunkFilename: 'static/js/[name].chunk.js',
     path: path.resolve(__dirname, 'dist'),
   },
-  module: {
-    rules: [
-      {
-        // css-loader
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader', 'postcss-loader'],
-      },
-      {
-        // sass/scss loader to load sass-scss style files
-        test: /\.(sass|scss)$/,
-        use: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader'],
-      },
-      {
-        // copies image files to assets folder in destination folder - dist
-        test: /\.(svg|png|jpg|jpeg|gif|mp3|ico)$/,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              name: '[name].[ext]',
-              outputPath: 'static/assets',
-            },
-          },
-        ],
-      },
-    ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: path.join(__dirname, 'public', 'index.html'),
-      favicon: path.resolve(__dirname, 'public', 'favicon.ico'),
-    }),
-    new webpack.HotModuleReplacementPlugin(),
-  ],
+  plugins: [new webpack.HotModuleReplacementPlugin()],
   devServer: {
     contentBase: path.resolve(__dirname, 'dist'),
     port: 3000,
     hot: true,
     stats: 'minimal',
   },
-})
+});
